b9cf845af95a94d10e9d42636d013fe9f5c35744
Vakil.Verma <vakil@nimapinfotech.com>
2024-02-04 18:10:45 +0530

    content updated

--- pages/component/Header.js
+++ pages/component/Header.js
@@ -77,6 +77,7 @@ const Header = () => {
                 <Link
                   href="/"
                   className="block py-2 pl-3 pr-4 text-green-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-bold text-base"
+                  data-collapse-toggle="navbar-default"
                 >
                   Home
                 </Link>
@@ -85,6 +86,7 @@ const Header = () => {
                 <Link
                   href="/treat"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-bold text-base"
+                  data-collapse-toggle="navbar-default"
                 >
                   WHAT WE TREAT
                 </Link>
