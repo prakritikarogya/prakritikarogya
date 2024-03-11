a0d4ad8861a38ca69de2f01c7d14c5878e167abc
Vakil.Verma <vakil@nimapinfotech.com>
2024-03-11 13:21:33 +0530

    seo detail added

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Head from "next/head";
 import About from "./component/About";
 import Service from "./component/Service";
 import Main from "./component/Main";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>prakritikarogyaashram</title>
-      </Head>
       <Main />
       <About />
       <Service />
