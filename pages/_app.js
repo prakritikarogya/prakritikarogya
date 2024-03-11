1b34100433837a737ed32a7821b880bb2def0e1e
Vakil.Verma <vakil@nimapinfotech.com>
2024-03-11 13:17:28 +0530

    seo detail added

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import '../styles/globals.css'
 import 'flowbite';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
    return (
       <>
-      {/* <Head>
-         <title></title>
-      </Head> */}
-      <div className="container mx-auto bg-gray-300">
-         <Header />
-         <Component {...pageProps} />
-         <Footer />
-      </div>
+         <Head>
+            <title>Pratkritik Arogyaashram: Nature Cure institute & Empowering Holistic Health Solutions</title>
+            <meta name="description" content="Discover Pratkritik Arogyaashram's compassionate approach to healthcare, integrating Naturopathy, Ayurveda, Yoga, and more for personalized well-being. Our expert team is dedicated to empowering individuals on their health journey, fostering vitality and resilience through holistic solutions. Join us in shaping a transformative healthcare experience for a healthier future." />
+            <meta name="keywords" content="Pratkritik Arogyaashram, compassionate healthcare, Naturopathy, Ayurveda, Yoga, personalized well-being, holistic solutions, vitality, resilience, transformative healthcare" />
+         </Head>
+         <div className="container mx-auto bg-gray-300">
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+         </div>
       </>)
 }
